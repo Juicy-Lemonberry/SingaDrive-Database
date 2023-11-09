21405d831331b6057f48724dab0aa88f3d3c9505
Yeo Wen Qin <wqyeo@outlook.com>
2023-11-09 19:16:55 +0800

    populated categories table with data

---
+++ postgreSQL-migrations/V4.0.9__Populate-Categories-Table.sql
@@ -0,0 +1,17 @@
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('Announcements', 'General Announcements', 1, 0);
+
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('FAQ', 'Important FAQs', 1, 1);
+
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('General', 'Discussions anything related to cars, or SingaDrive rental service.', 2, 0);
+
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('Support', 'Ask for general related support here.', 2, 1);
+
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('Introduction', 'New here? Introduce yourself here!', NULL, 0);
+
+INSERT INTO "forum"."categories" ("title", "description", "section_id", "ordering")
+VALUES ('Random', 'Any discussion that is out of scope goes here. (Yes, includes non-car related discussions!)', NULL, 1);
