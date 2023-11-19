391d7f3c8d221c2c5e200c77c9cf588835111fab
Yeo Wen Qin <wqyeo@outlook.com>
2023-11-20 01:48:46 +0800

    SQL Function to delete a post

---
+++ postgreSQL-migrations/V4.3.7__Delete-Post.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION "forum"."delete_post"(
+    p_session_token VARCHAR,
+    p_browser_info TEXT,
+    p_post_id INT
+)
+RETURNS TABLE (
+	message TEXT
+)
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+DECLARE
+    l_account_id INT;
+BEGIN
+    -- Validate user credentials
+    SELECT account_id INTO l_account_id
+    FROM "user"."check_session_token"(p_session_token, p_browser_info);
+
+    -- Check if the account_id was returned
+    IF l_account_id IS NOT NULL THEN
+        -- Check if the user owns the post or has deletion rights
+        IF EXISTS (
+            SELECT 1
+            FROM "forum"."posts"
+            WHERE "id" = p_post_id AND "account_id" = l_account_id
+        ) THEN
+            -- Delete the post
+            DELETE FROM "forum"."posts"
+            WHERE "id" = p_post_id;
+            message := 'SUCCESS';
+        ELSE
+            -- User does not own the post or lacks deletion rights
+            message := 'FAILURE';
+        END IF;
+    ELSE
+        -- User validation failed
+        message := 'USER';
+    END IF;
+	RETURN NEXT;
+END;
+$$;
