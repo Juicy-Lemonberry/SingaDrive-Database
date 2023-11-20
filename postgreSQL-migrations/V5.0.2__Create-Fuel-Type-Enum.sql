035051e8bedd8d14868517966a3885f408a03e7e
Yeo Wen Qin <wqyeo@outlook.com>
2023-11-20 15:23:32 +0800

    Enum for fuel types

---
+++ postgreSQL-migrations/V5.0.2__Create-Fuel-Type-Enum.sql
@@ -0,0 +1,12 @@
+CREATE TYPE automobile.fuel_type AS ENUM (
+  'Diesel',
+  'Electric',
+  'Gas',
+  'Hybrid',
+  'Plug-in Hybrid',
+  'Hydrogen Fuel Cell',
+  'Compressed Natural Gas',
+  'Liquefied Petroleum Gas',
+  'Flex-Fuel',
+  'Bio-Diesel'
+);
